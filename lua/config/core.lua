23b551524e538991cacd6fa02113024705c7396b
hex4coder <the.programmer.luyo@gmail.com>
2025-03-07 21:36:21 +0800

    Add alpha plugin for dashboard

--- lua/config/core.lua
+++ lua/config/core.lua
@@ -1,3 +1,6 @@
 vim.g.mapleader = ' '
 vim.opt.termguicolors = true
 vim.opt.clipboard = 'unnamedplus'
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -1,15 +1,14 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-	require("neo-tree").setup({})
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+    },
+    keys = {
 
-	local keymap = vim.keymap
-	keymap.set('n', '<leader>ee', '<cmd>Neotree toggle<CR>', {desc = "File explorer using neo tree", silent = true, noremap = true})
-  end
+        {'<leader>ee', '<cmd>Neotree toggle<CR>', desc = "File explorer using neo tree", silent = true, noremap = true}
+    },
+    opts = {},
 }
