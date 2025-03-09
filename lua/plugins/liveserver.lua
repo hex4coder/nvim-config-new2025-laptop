d8b4dd22332caf453b74af04715338dd26b78d07
hex4coder <the.programmer.luyo@gmail.com>
2025-03-09 13:43:46 +0800

    Add liveserver

---
+++ lua/plugins/liveserver.lua
@@ -0,0 +1,16 @@
+return {
+    "barrett-ruth/live-server.nvim",
+    build = "npm add -g live-server",
+    cmd = { "LiveServerStart", "LiveServerStop" },
+    config = function()
+        require("live-server").setup({})
+
+        local keymap = vim.keymap
+        local opts = { silent = true, noremap = true }
+
+        opts.desc = "Start liveserver"
+        keymap.set("n", "<leader>st", "<cmd>LiveServerStart<CR>", opts)
+        opts.desc = "Stop liveserver"
+        keymap.set("n", "<leader>sp", "<cmd>LiveServerStop<CR>", opts)
+    end,
+}
