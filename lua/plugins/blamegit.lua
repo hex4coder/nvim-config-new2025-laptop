c69d6432fbcb8b871145b0e9d2e32be6a7b7ad78
hex4coder <the.programmer.luyo@gmail.com>
2025-03-09 13:28:00 +0800

    Add another blame plugins options

--- lua/plugins/blamegit.lua
+++ lua/plugins/blamegit.lua
@@ -1,21 +1,32 @@
 return {
-	{
-		"FabijanZulj/blame.nvim",
-		lazy = false,
-		config = function()
-			require("blame").setup({})
-			vim.keymap.set(
-				"n",
-				"<leader>bw",
-				"<cmd>BlameToggle window<CR>",
-				{ desc = "Toggle blame git mode window", noremap = true, silent = true }
-			)
-			vim.keymap.set(
-				"n",
-				"<leader>bm",
-				"<cmd>BlameToggle virtual<CR>",
-				{ desc = "Toggle blame git mode virtual", noremap = true, silent = true }
-			)
-		end,
-	},
+    {
+        "FabijanZulj/blame.nvim",
+        event = { "BufEnter" },
+        lazy = false,
+        config = function()
+            require("blame").setup({})
+            vim.keymap.set(
+                "n",
+                "<leader>bw",
+                "<cmd>BlameToggle window<CR>",
+                { desc = "Toggle blame git mode window", noremap = true, silent = true }
+            )
+            vim.keymap.set(
+                "n",
+                "<leader>bm",
+                "<cmd>BlameToggle virtual<CR>",
+                { desc = "Toggle blame git mode virtual", noremap = true, silent = true }
+            )
+        end,
+    },
+    {
+        "APZelos/blamer.nvim",
+        event = { "BufEnter" },
+        config = function()
+            vim.g.blamer_enabled = true
+            vim.g.blamer_delay = 1000
+            vim.g.blamer_prefix = " > "
+            vim.g.blamer_date_format = "%d/%m/%y"
+        end,
+    },
 }
